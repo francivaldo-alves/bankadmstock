7b356640eaf02fa13446747053c734096c23c759
Francivaldo Alves <francivaldo.alves@gmail.com>
2022-04-15 09:56:03 -0300

    Implementação do CRUD na CategoryService

---
+++ bank-admstock/src/main/java/com/f3pro/bankadmstock/services/CategoryService.java
@@ -0,0 +1,49 @@
+package com.f3pro.bankadmstock.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.f3pro.bankadmstock.entities.Category;
+import com.f3pro.bankadmstock.repositories.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+//listar tudo 
+    public List<Category> findAll(){
+        return repository.findAll();
+        
+
+    }
+
+    // listar por ID
+    public Optional<Category> findById(Long id){
+        Optional<Category> obj = repository.findById(id);
+        return obj;
+
+    }
+    //Criar um categoria
+    public Category  create(Category category){
+
+        return repository.save(category);
+    }
+
+    // atualizar uma categoria
+    public Category update(Category category, Long id){
+        category.setId(id);
+        return repository.save(category);
+
+    }
+
+    //deletar a categoria por ID.
+    public void delete(Long id){
+
+        repository.deleteById(id);
+    }
+}
